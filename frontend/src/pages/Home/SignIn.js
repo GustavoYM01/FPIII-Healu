6d4ccf5f141ad92005ae5f519d996271af7f0c12
GustavoYM01 <gumatimoto@hotmail.com>
2022-10-18 14:36:01 -0300

    added animated scroll-down in mobile login screen

--- frontend/src/pages/Home/SignIn.js
+++ frontend/src/pages/Home/SignIn.js
@@ -37,60 +37,116 @@ const SignIn = () => {
   }, [authError]);
 
   return (
-    <div className="form_container">
-      <div id="divLogo_title">
-        <div>
-          <img id="logo_form" src={logoForm} alt="logo do formulário" />
-          <h1 id="logo_title">HEALU</h1>
-        </div>
-      </div>
-      <div id="divForm">
-        <h2>
-          Lorem ipsum dolor
-        </h2>
-        <div>
-          <form className="form_signin" onSubmit={handleSubmit}>
-            <input
-              type="email"
-              id="input_email"
-              name="email"
-              required
-              placeholder="Digite o seu email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="password"
-              id="input_password"
-              name="password"
-              required
-              placeholder="Digite a sua senha"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <div className="forgot_pass">
-              <a href="/esquecisenha">Esqueci a senha</a>
+    <>
+      {window.innerHeight <= 568 ? (
+        <div className="form_container">
+          <div id="divLogo_title">
+            <div>
+              <img id="logo_form" src={logoForm} alt="logo do formulário" />
+              <div class="scroll-down"></div>
+              <h1 id="logo_title">HEALU</h1>
             </div>
-            {!loading && (
-              <input type="submit" id="input_submit" value="Login" />
-            )}
-            {loading && (
-              <input
-                type="submit"
-                id="input_submit"
-                disabled
-                value="aguarde..."
-              />
-            )}
-          </form>
-          <div id="after_form">
-            <p>Não possui conta?</p>
-            <a href="/cadastrar">Cadastre-se</a>
           </div>
-          {error && <p className="error">{error}</p>}
+          <div id="divForm">
+            <h2>Lorem ipsum dolor</h2>
+            <div>
+              <form className="form_signin" onSubmit={handleSubmit}>
+                <input
+                  type="email"
+                  id="input_email"
+                  name="email"
+                  required
+                  placeholder="Digite o seu email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="password"
+                  id="input_password"
+                  name="password"
+                  required
+                  placeholder="Digite a sua senha"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <div className="forgot_pass">
+                  <a href="/esquecisenha">Esqueci a senha</a>
+                </div>
+                {!loading && (
+                  <input type="submit" id="input_submit" value="Login" />
+                )}
+                {loading && (
+                  <input
+                    type="submit"
+                    id="input_submit"
+                    disabled
+                    value="aguarde..."
+                  />
+                )}
+              </form>
+              <div id="after_form">
+                <p>Não possui conta?</p>
+                <a href="/cadastrar">Cadastre-se</a>
+              </div>
+              {error && <p className="error">{error}</p>}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="form_container">
+          <div id="divLogo_title">
+            <div>
+              <img id="logo_form" src={logoForm} alt="logo do formulário" />
+              <h1 id="logo_title">HEALU</h1>
+            </div>
+          </div>
+          <div id="divForm">
+            <h2>Lorem ipsum dolor</h2>
+            <div>
+              <form className="form_signin" onSubmit={handleSubmit}>
+                <input
+                  type="email"
+                  id="input_email"
+                  name="email"
+                  required
+                  placeholder="Digite o seu email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="password"
+                  id="input_password"
+                  name="password"
+                  required
+                  placeholder="Digite a sua senha"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <div className="forgot_pass">
+                  <a href="/esquecisenha">Esqueci a senha</a>
+                </div>
+                {!loading && (
+                  <input type="submit" id="input_submit" value="Login" />
+                )}
+                {loading && (
+                  <input
+                    type="submit"
+                    id="input_submit"
+                    disabled
+                    value="aguarde..."
+                  />
+                )}
+              </form>
+              <div id="after_form">
+                <p>Não possui conta?</p>
+                <a href="/cadastrar">Cadastre-se</a>
+              </div>
+              {error && <p className="error">{error}</p>}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
